8a93e7c19b26a2d4dc5fbe7e82d1074d34b4e048
Habtetsega <kido3871@gimail.com>
2023-02-16 20:56:21 -0800

    advanced alx c project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - Prints "with proper grammar, but the outcome is
- *   *                a piece of art," followed by a new line.
- *    *
- *     * Return: Always 0.
- *      */
+ * main - Prints "with proper grammar, but the outcome is
+ * a piece of art," followed by a new line.
+ * Return: Always 0.
+ */
 int main(void)
 {
 			printf("with proper grammar, but the outcome is a piece of art,\n");
